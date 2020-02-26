31ccf8ef45f170bbae2ba7cc671f81ae57b7be51
Larry <blarry1013@gmail.com>
2020-02-26 19:03:21 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'nutrient.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '均衡飲食',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: '均衡飲食'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void getDailyMealDetail() {
+    setState(() {
+      Nutrient fakeNutrient = Nutrient(fruit: 1, fat: 2, carbohydrate: 3, water: 4, protein: 5);
+      print(fakeNutrient.protein);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '今日還沒有任何飲食記錄唷 :)',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getDailyMealDetail,
+        tooltip: 'Increment',
+        child: Icon(Icons.fastfood),
+      ),
+    );
+  }
+}
---
+++ lib/nutrient.dart
@@ -0,0 +1,9 @@
+class Nutrient {
+  // 數字多少代表吃了幾分  
+  int carbohydrate;
+  int water;
+  int fat;
+  int protein;
+  int fruit;
+  Nutrient({this.carbohydrate, this.fat, this.fruit, this.protein, this.water});
+}
