092879f9a399959a5a1893a12f13d4938115ff43
Larry <blarry1013@gmail.com>
2020-02-26 19:16:55 +0800

    init add meal widget

---
+++ lib/createMeal.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class CreateMeal extends StatefulWidget{
+  @override
+  _CreateMealStates createState() => _CreateMealStates();
+}
+
+class _CreateMealStates extends State<CreateMeal> {
+    @override 
+    Widget build(BuildContext context) {
+      return Text('AA');
+    }
+}
